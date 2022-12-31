d1f13608466ee902e06ba1653a9b0ee0e2248505
Swayam rajkumar karle <99035115+DevSwayam@users.noreply.github.com>
2023-01-01 00:50:11 +0530

    first commit

---
+++ block.js
@@ -0,0 +1,22 @@
+import {GENESIS_DATA} from "./config.js";
+
+class Block{
+    constructor({timestamp,prevHash,hash,data}){
+        this.timestamp = timestamp;
+        this.prevHash = prevHash;
+        this.hash = hash;
+        this.data = data;
+    }
+    static genesis(){
+        return new this(GENESIS_DATA);
+    }
+}
+
+
+const block1 = new Block({timestamp : '1/1/1' , 
+                            prevHash :'0x' , 
+                            hash :'0x' ,
+                            data :'hello'
+                        });
+const genesisBlock = Block.genesis();
+console.log(genesisBlock);
---
+++ config.js
@@ -0,0 +1,7 @@
+export  const GENESIS_DATA ={
+        timestamp:'1/1/2023;',
+        prevHash: '0x000', 
+        hash:'0x123',
+        data:[]
+}
+
