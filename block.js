fd9bc3966f17350273681637faf9d3bf006c697b
Swayam rajkumar karle <99035115+DevSwayam@users.noreply.github.com>
2023-01-02 04:20:08 +0530

    added hashing functionality to DevChain

--- block.js
+++ block.js
@@ -1,23 +1,31 @@
-import {GENESIS_DATA} from "./config.js";
+
+import {GENESIS_BLOCK} from "./config.js"; // imported our genesis block 
 
 class Block{
+
+    // Every block need to be intialize with below values 
     constructor({timestamp,prevHash,hash,data}){
         this.timestamp = timestamp;
         this.prevHash = prevHash;
         this.hash = hash;
         this.data = data;
     }
+
+    // as genesis block is a static data and wont change hence we use static method
     static genesis(){
-        return new this(GENESIS_DATA);
+        return new this(GENESIS_BLOCK); // whenever someone calls it will return this genesis block data
     }
+
+    
 }
 
-
+//creating a sample block
 const block1 = new Block({timestamp : '1/1/1' , 
                             prevHash :'0x' , 
                             hash :'0x' ,
                             data :'hello'
                         });
-const genesisBlock = Block.genesis();
+
+const newGenesis = Block.genesis();
 console.log(block1);
-console.log(genesisBlock);
+console.log(newGenesis);
--- config.js
+++ config.js
@@ -1,7 +1,10 @@
-export  const GENESIS_DATA ={
+// this is genesis block and wont chage
+
+export  const GENESIS_BLOCK ={
         timestamp:'1/1/2023;',
         prevHash: '0x000', 
         hash:'0x123',
-        data:[]
+        data:[],
+        owner:'DevSwayam'
 }
 
