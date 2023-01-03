010c5c42f8de8799d98ab7a2cb9cc5a508397eea
Swayam rajkumar karle <99035115+DevSwayam@users.noreply.github.com>
2023-01-03 15:01:19 +0530

    wasted

--- block.js
+++ block.js
@@ -1,54 +1,66 @@
 
-import {GENESIS_BLOCK} from "./config.js"; // imported our genesis block 
-import {cryptoHash} from "./crypto-hash.js";
-import {difficulty} from "./config.js";
+const hexToBinary = require("hex-to-binary");
+const { GENESIS_DATA, MINE_RATE } = require("./config");
+const cryptoHash = require("./crypto-hash");
+class Block {
+  constructor({ timestamp, prevHash, hash, data, nonce, difficulty }) {
+    this.timestamp = timestamp;
+    this.prevHash = prevHash;
+    this.hash = hash;
+    this.data = data;
+    this.nonce = nonce;
+    this.difficulty = difficulty;
+  }
+  static genesis() {
+    return new this(GENESIS_DATA);
+  }
+  static mineBlock({ prevBlock, data }) {
+    let hash, timestamp;
+    const prevHash = prevBlock.hash;
+    let { difficulty } = prevBlock;
 
-export class Block{
+    let nonce = 0;
+    do {
+      nonce++;
+      timestamp = Date.now(); //00cdef ,00
+      difficulty = Block.adjustDifficulty({
+        originalBlock: prevBlock,
+        timestamp,
+      });
+      hash = cryptoHash(timestamp, prevHash, data, nonce, difficulty);
+    } while (
+      hexToBinary(hash).substring(0, difficulty) !== "0".repeat(difficulty)
+    );
+    return new this({
+      timestamp,
+      prevHash,
+      data,
+      difficulty,
+      nonce,
+      hash,
+    });
+  }
 
-    // Every block need to be intialize with below values 
-    constructor({timestamp,prevHash,hash,data,nonce,difficulty}){
-        this.timestamp = timestamp;
-        this.prevHash = prevHash;
-        this.hash = hash;
-        this.data = data;
-        this.nonce = nonce;
-        this.difficulty = difficulty;
-    }
-
-    // as genesis block is a static data and wont change hence we use static method
-    static genesis(){
-        return new this(GENESIS_BLOCK); // whenever someone calls it will return this genesis block data
-    }
-
-    static mineBlock({prevBlock,data}){ // will take inputs and mine block
-        let hash,timestamp;
-        let nonce =0;
-        //const timestamp = Date.now(); // current date
-        const prevHash = prevBlock.hash; // take previous block hash as current block prevHash
-        do {
-            nonce++; // keep increasing nonce till you get hash of certain difficulty
-            timestamp=Date.now(); // constantlly updating timestamp of when block is going to get created
-            hash=cryptoHash(timestamp,prevHash,data,nonce,difficulty);
-        } while (hash.substring(0,difficulty) !== '0'.repeat(difficulty));// jab tak hash ke first two character 00 na ho jaye tab tak keep finding hash
-        return new Block({ // will call constructor of this class function and constructor will get all this arguments and block will be created  
-            timestamp,
-            prevHash,
-            data,
-            difficulty,
-            nonce,
-            hash,
-        })
-    }
-    
+  static adjustDifficulty({ originalBlock, timestamp }) {
+    const { difficulty } = originalBlock;
+    if (difficulty < 1) return 1;
+    const difference = timestamp - originalBlock.timestamp;
+    if (difference > MINE_RATE) return difficulty - 1;
+    return difficulty + 1;
+  }
 }
 
-//creating a sample block
-const block1 = new Block({timestamp : '1/1/1' , 
-                            prevHash :'0x' , 
-                            hash :'0x' ,
-                            data :'hello'
-                        });
+const block1 = new Block({
+  hash: "0xacb",
+  timestamp: "2/09/22",
+  prevHash: "0xc12",
+  data: "hello",
+});
 
-/*
-const newGenesis = Block.genesis();
-console.log(newGenesis); */
+// const genesisBlock = Block.genesis();
+// console.log(genesisBlock);
+
+// const result = Block.mineBlock({ prevBlock: block1, data: "block2" });
+// console.log(result);
+// //console.log(block1);
+module.exports = Block;
--- config.js
+++ config.js
@@ -1,12 +1,11 @@
-// this is genesis block and wont chage
-export const difficulty = 2;
-export  const GENESIS_BLOCK ={
-        timestamp:'1/1/2023;',
-        prevHash: '0x000', 
-        hash:'0x123',
-        data:[],
-        owner:'DevSwayam',
-        nonce:0,
-        difficulty:difficulty
-}
-
+const MINE_RATE = 1000; //1s = 1000ms
+const INITIAL_DIFFICULTY = 2;
+const GENESIS_DATA = {
+  timestamp: 1,
+  prevHash: "0x000",
+  hash: "0x123",
+  difficulty: INITIAL_DIFFICULTY,
+  nonce: 0,
+  data: [],
+};
+module.exports = { GENESIS_DATA, MINE_RATE };
