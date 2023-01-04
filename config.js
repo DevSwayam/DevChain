61c4d38ca2026c68ea93581d6eba206499a5268e
Swayam rajkumar karle <99035115+DevSwayam@users.noreply.github.com>
2023-01-04 16:06:38 +0530

    small changes

--- config.js
+++ config.js
@@ -1,18 +1,4 @@
-<<<<<<< HEAD
-// this is genesis block and wont chage
-export const mining_rate = 1000; // 1s = 1000ms 
-const INITIAL_DIFFICULTY = 2;
-export  const GENESIS_BLOCK ={
-        timestamp:'1/1/2023;',
-        prevHash: '0x000', 
-        hash:'0x123',
-        data:[],
-        owner:'DevSwayam',
-        nonce:0,
-        difficulty:INITIAL_DIFFICULTY
-}
 
-=======
 const MINE_RATE = 1000; //1s = 1000ms
 const INITIAL_DIFFICULTY = 2;
 const GENESIS_DATA = {
@@ -24,4 +10,3 @@ const GENESIS_DATA = {
   data: [],
 };
 module.exports = { GENESIS_DATA, MINE_RATE };
->>>>>>> 010c5c42f8de8799d98ab7a2cb9cc5a508397eea
